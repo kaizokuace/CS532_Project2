197c555b1698968aed1072e8c1eeeda17ea4b2fb
Hasen Ahmad <kaizokuace@gmail.com>
2012-03-12 22:44:24 -0700

    added Node and HuffTree business
    still not down implementing HuffTree, Node is done for now.

--- src/com/hasenahmad/HuffTree.java
+++ src/com/hasenahmad/HuffTree.java
@@ -1,5 +1,39 @@
 package com.hasenahmad;
 
-public class HuffTree {
-	//TODO: get started!
+import java.util.*;
+
+public class HuffTree{
+	//take in an array of Value class objects (this contains the value and frequency data pair)
+	//sort this array from low to high
+	//take the 2 lowest
+	
+	//pop 2, combine, push back, sort
+	//ArrayList<Node> listy = new ArrayList<Node>();
+	//Collections.sort
+	private Stack<Node> stacky;
+	
+	public HuffTree(ArrayList<Node> nodeList){
+		stacky = new Stack<Node>();
+		Iterator<Node> itr = nodeList.iterator();
+		while(itr.hasNext()){
+			stacky.push(itr.next());
+		}
+		System.out.println(stacky);
+		while(!stacky.empty())
+		Collections.sort(stacky);
+		System.out.println(stacky);
+		
+		stacky.push(combineLow(stacky.pop(), stacky.pop()));
+	}
+
+	private Node combineLow(Node a, Node b) {
+		Node newRoot = new Node('*', a.getFrequency() + b.getFrequency());
+		newRoot.setLeft(a);
+		newRoot.setRight(b);
+		return newRoot;
+	}
+	
+
+	
+	
 }
---
+++ src/com/hasenahmad/Node.java
@@ -0,0 +1,54 @@
+package com.hasenahmad;
+
+public class Node implements Comparable<Node> {
+	private char value;
+	private int frequency;
+	private Node left;
+	private Node right;
+	public Node(char value, int frequency){
+		this.setValue(value);
+		this.setFrequency(frequency);
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(char value) {
+		this.value = value;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		if (getFrequency() > ((Node)o).getFrequency())
+			return 1;
+		else if (getFrequency() < ((Node)o).getFrequency())
+			return -1; 
+		else
+			return 0;
+	}
+}
