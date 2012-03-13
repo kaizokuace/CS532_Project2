9f8d1c74092e620f7a65db55d00b5a181f96db63
Hasen Ahmad <kaizokuace@gmail.com>
2012-03-13 00:48:38 -0700

    added some input test data.
    Need to implement file input and parsing.

--- src/com/hasenahmad/HuffPuff.java
+++ src/com/hasenahmad/HuffPuff.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class HuffPuff {
 	public static ArrayList<Node> listy = new ArrayList<Node>();
-	
+	public static ArrayList<Node> listy2 = new ArrayList<Node>();	
 
 	public static void main(String[] args) {
 		listy.add(new Node('A', 3));
@@ -12,9 +12,21 @@ public class HuffPuff {
 		listy.add(new Node('C', 2));
 		listy.add(new Node('D', 2));
 		HuffTree huff = new HuffTree(listy);
-		String string = "";
-		huff.getCode(huff.getRoot(), string);
 		System.out.println("Listy: " + listy);
+		
+		listy2.add(new Node('A', 20));
+		listy2.add(new Node('B', 2));
+		listy2.add(new Node('C', 5));
+		listy2.add(new Node('D', 15));
+		listy2.add(new Node('E', 10));
+		listy2.add(new Node('F', 100));
+		listy2.add(new Node('G', 80));
+		listy2.add(new Node('H', 175));
+		listy2.add(new Node('I', 150));
+		listy2.add(new Node('J', 6));
+		listy2.add(new Node('K', 100));
+		HuffTree huff2 = new HuffTree(listy2);
+		System.out.println("Listy: " + listy2);
 	}
 
 }
--- src/com/hasenahmad/HuffTree.java
+++ src/com/hasenahmad/HuffTree.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class HuffTree{
 	//pop 2, combine, push back, sort
 	private Stack<Node> stacky;
+	private String stringy = "";
 	
 	public HuffTree(ArrayList<Node> nodeList){
 		stacky = new Stack<Node>();
@@ -12,16 +13,16 @@ public class HuffTree{
 		while(itr.hasNext()){
 			stacky.push(itr.next());
 		}
-		System.out.println(stacky);
+		//System.out.println(stacky);
 		while(stacky.size() > 2){
 			Collections.sort(stacky);
 			Collections.reverse(stacky);
-			System.out.println(stacky);
+			//System.out.println(stacky);
 			stacky.push(combineLow(stacky.pop(), stacky.pop()));
 		}
 		stacky.push(combineLow(stacky.pop(), stacky.pop()));
-			System.out.println(stacky);
-
+			//System.out.println(stacky);
+			getCode(getRoot(), stringy);
 	}
 
 	private Node combineLow(Node a, Node b) {
@@ -31,18 +32,17 @@ public class HuffTree{
 		return newRoot;
 	}
 	
-	public Node getRoot(){
+	private Node getRoot(){
 		return stacky.peek();
 	}
 	
-	public void getCode(Node T, String S){
+	private void getCode(Node T, String S){
 	    if (T.getValue() != '*') {
 	        //the code for the character in the node T is now in string S, 
 	        //so tuck this info away somewhere;
 	    	T.setCode(S);
 	        return;
 	    }
-
 	    getCode(T.getLeft(), S + "0");
 	    getCode(T.getRight(), S + "1");
 	}
